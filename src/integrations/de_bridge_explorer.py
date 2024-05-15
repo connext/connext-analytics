af90c15a2efe9826815b462de5a3258e661f8b24
Jaynaik19 <jaynaik94@gmail.com>
2024-05-15 12:18:58 +0530

    reports and debridge explorer repull for bugs issue

--- src/integrations/de_bridge_explorer.py
+++ src/integrations/de_bridge_explorer.py
@@ -1,3 +1,4 @@
+import time
 import logging
 import pandas_gbq as gbq
 import requests
@@ -77,16 +78,29 @@ def post_deexplorer_data_call(skip=0):
     Returns:
         _type_: _description_
     """
+    # data = {
+    #     "giveChainIds": [],
+    #     "takeChainIds": [],
+    #     "orderStates": ["Fulfilled"],
+    #     # "externalCallStates": ["Completed"],
+    #     "externalCallStates": [],
+    #     "skip": skip,
+    #     "take": 100,
+    # }
     data = {
         "giveChainIds": [],
         "takeChainIds": [],
-        "orderStates": [],
-        "externalCallStates": ["Completed"],
+        "orderStates": ["Fulfilled", "SentUnlock", "ClaimedUnlock"],
         "skip": skip,
         "take": 100,
     }
+    logging.info(f"Fetching orders: skip={skip}")
     deexplorer = requests.post(url, headers=headers, data=json.dumps(data))
-    deexplorer = json.loads(deexplorer.text)
+    try:
+        deexplorer = json.loads(deexplorer.text)
+    except json.JSONDecodeError:
+        pass
+
     if "orders" in deexplorer:
         actual = deexplorer["orders"]
     else:
@@ -95,29 +109,53 @@ def post_deexplorer_data_call(skip=0):
 
     deexplorer_data = pd.json_normalize(actual)
     deexplorer_data = deexplorer_data.filter(items=necessary)
-    return deexplorer_data
+
+    deexplorer_data["creationTimestamp"] = deexplorer_data["creationTimestamp"].astype(
+        int
+    )
+    return deexplorer_data.drop_duplicates()
+
+
+def get_skip_count_from_bq():
+
+    query = "SELECT COUNT(1) AS max FROM `mainnet-bigq.raw.source_de_bridge_explorer__transactions` "
+    skip = gbq.read_gbq(query, project_id=PROJECT_ID).iloc[0]["max"]
+    return skip
 
 
 # convert the below into a function
-def get_deexplorer_transactions(skip=0):
+def get_deexplorer_transactions():
+
+    # skip = get_skip_count_from_bq()
+    skip = 0
 
     all_orders_df = pd.DataFrame()
 
-    # Initialize skip value
-    skip = 0
+    total_orders = 1000000
 
     # Loop to fetch all orders
-    while True:
-        print(f"Fetching orders with skip={skip}...")
+    while total_orders > 0:
         df_deexplorer = post_deexplorer_data_call(skip=skip)
         if df_deexplorer.empty:
-            print("No more orders to fetch.")
+
+            time.sleep(60)
+            skip += 100
+            logging.info(
+                "No more orders to fetch. Sleeping for 60 seconds before next call"
+            )
+            continue
+
+        # break if this years data is pulled
+        if df_deexplorer["creationTimestamp"].min() < 1704067200:
+            logging.info("This years data is pulled. Stopping...")
             break
+
         all_orders_df = pd.concat([all_orders_df, df_deexplorer], ignore_index=True)
         skip += 100
+        total_orders -= 100
+        logging.info(f"Total orders remaining: {total_orders}")
 
     # CLean before upload
-
     # convert the column names from camelCase to snake_case
     all_orders_df.columns = [
         col.lower().replace(".", "_") for col in all_orders_df.columns
@@ -136,7 +174,7 @@ def de_bridge_explorer_pipeline():
         dataframe=df,
         project_id=PROJECT_ID,
         destination_table="raw.source_de_bridge_explorer__transactions",
-        if_exists="append",
+        if_exists="replace",
         chunksize=10000,
         api_method="load_csv",
     )
@@ -146,5 +184,4 @@ if __name__ == "__main__":
 
     # TODO: Adding a schedule to run this pipeline
     # TODO: Uploading data that is new to the raw table
-    # de_bridge_explorer_pipeline()
-    print("Check TODO")
+    de_bridge_explorer_pipeline()

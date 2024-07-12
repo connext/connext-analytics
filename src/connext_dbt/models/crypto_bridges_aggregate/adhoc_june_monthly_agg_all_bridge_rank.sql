b061a93a0852f4ac537d1cd9daefee20966b5519
Jaynaik19 <jaynaik94@gmail.com>
2024-07-12 11:41:25 +0530

    formula adjustment

--- src/connext_dbt/models/crypto_bridges_aggregate/adhoc_june_monthly_agg_all_bridge_rank.sql
+++ src/connext_dbt/models/crypto_bridges_aggregate/adhoc_june_monthly_agg_all_bridge_rank.sql
@@ -7,8 +7,8 @@ SELECT
   SUM(b.total_volume) AS Volume,
   AVG(b.avg_volume) AS avg_tx_size,
   SUM(b.total_txs) AS no_of_txs,
-  SUM(tx_count_10k_txs) AS no_of_10k_txs,
-  100 * SUM(tx_count_10k_txs) / SUM(b.total_txs) AS Share_of_10k_transfers
+  SUM(b.volume_10k_txs) AS volume_of_10k_txs,
+  100 * SUM(b.volume_10k_txs) / SUM(b.total_volume) AS Share_of_10k_transfers
 
 FROM `mainnet-bigq.crypto_bridges_aggregate.monthly_agg_all_bridge` b
 WHERE destination_chain_name IS NOT NULL AND b.date >= '2024-06-01'
